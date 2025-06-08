c11b358416242c8dccf24d25c1309a3b7e62cf76
Shina shamsedin <nadiyariddle1998@gmail.com>
2025-06-08 10:27:22 +0330

    Update sensor_display.cpp

--- sensor_display.cpp
+++ sensor_display.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <cstdio>
 
-void displayFormatted(char* data) {
+#void (displayFormatted)(char* data) 
+{
     // Assume data is like: GAS:123,TEMP:30,S3:500
     printf("[Sensor Readings]\n------------------\n%s\n\n", data);
 }
 
-int main() {
+#int main() {
     // Sample sensor data
-    char sensorData[] = "GAS:123,TEMP:30,S3:500";
+    #char sensorData[] = "GAS:123,TEMP:30,S3:500";
 
-    displayFormatted(sensorData);
+    displayFormatted(sensorData);}
+{
 
     return 0;
 }
