09523cfce30fce18bfff0766047df3ef6311c91c
hosinzadh <amirbelk3439@gmail.com>
2025-06-26 22:51:01 +0330

    Add files via upload

--- Arduino_–_تولید_و_ارسال_داده_به_سرور.cpp
+++ Arduino_–_تولید_و_ارسال_داده_به_سرور.cpp
@@ -1,6 +1,7 @@
-  // ساخت داده JSON به صورت نمونه
+// Create sample JSON data
   String sensorData = "{"value":" + String(random(20, 100)) + "}";
   webSocket.sendTXT(sensorData);
 
-  delay(2000); // ارسال هر 2 ثانیه
+  delay(2000);   // Send every 2 seconds
+
 }
